55a7e3bde5056382988e016d41f2f623380ae281
ValerioGc <valerio.gc1@gmail.com>
2022-11-17 14:56:47 +0100

    Avanzamneto

--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -1,7 +1,12 @@
 package org.generation.italy;
 
+import java.util.Random;
+
 public class Main {
 	public static void main(String[] args) {
-		
+		Product pr1 = new Product("Prodotto 1", "Descrizione prodotto 1", 10);
+		Product pr2 = new Product("Prodotto 2", "Descrizione prodotto 2", 34);
+		System.out.println(pr1 + "\n------------");
+		System.out.println(pr2);
 	}
 }
--- src/org/generation/italy/Product.java
+++ src/org/generation/italy/Product.java
@@ -1,5 +1,38 @@
 package org.generation.italy;
 
-public class Product {
+import java.util.Random;
 
+public class Product {
+	
+	int code;
+	String name;
+	String description;
+	int price;
+	char eur = '\u20ac';
+	int tax = 20;
+	
+	public Product(String name, String description, int price) {
+		this.name = name;
+		this.description = description;
+		this.price = price;	
+	}
+// Random code generator
+	public int getRndCode() {
+		Random rnd = new Random();
+		int code = rnd.nextInt(89999) + 10000;
+		return code;
+	}
+// Taxes price calculator 
+	public float afterTaxesPrice() {
+		float taxPrices = this.price + ((this.price * 20) / 100); 
+		return taxPrices;
+	}
+	
+	@Override
+	public String toString() {
+		return  "Nome: " + name + "\nCodice: " + getRndCode() +
+				"\nDescrizione: " + description + "\nPrezzo: " + 
+				price + " " + eur + "\nPrezzo con IVA: " + afterTaxesPrice() + " " + eur;
+	}
+	
 }
